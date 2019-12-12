ded3f47c011dde835b3cd1b4ddb6429bcd0a90a3
Cassie <cassandra.sloan16@gmail.com>
2019-12-12 14:58:21 +1100

    server running successfully

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-//dependencies/packages
-const express = require("express");
-const exphbs = require("express-handlebars");
-const mongoose = require("mongoose");
+const express = require('express') 
+const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 const routes = require("./routes")
-const port = 3000;
-const app = express();
+const app = express() 
+const port = 3000 
 
 //database
 mongoose.connect("mongodb://localhost/cat_tracker_api_practice", {
@@ -14,16 +13,15 @@ mongoose.connect("mongodb://localhost/cat_tracker_api_practice", {
 mongoose.Promise = global.Promise;
 mongoose.connection.on("error", error => console.log(error));
 
-//view engine (handlebars)
-app.engine("handlebars", exphbs({ defaultLayout : "main"}));
-app.set("view engine", "handlebars");
+//view engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' })) 
+app.set('view engine', 'handlebars') 
 
-//data formatting
-app.use(express.urlencoded, {extended:false});
-app.use(express.json());
+//data format
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 //routes
-app.use(routes);
+app.use(routes)
 
-//set up listening on port
-app.listen(port, () => console.log(`server is listening on port ${port}`));
+app.listen(port, () => console.log(`Server listening on port ${port}!`))
